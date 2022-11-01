c8df0895c8606028f2b54ea54da5ba286adbc696
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2022-11-01 15:20:46 +0200

    starter files

---
+++ src/main/java/com/kabelo/employeemanager/model/Employee.java
@@ -0,0 +1,10 @@
+package com.kabelo.employeemanager.model;
+
+public class Employee {
+    private Long id;
+    private String name;
+    private String email;
+    private String jobTitle;
+    private String phone;
+    private String imageUrl;
+}
